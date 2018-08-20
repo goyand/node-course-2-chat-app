eef54358eb8d06373d9df32c3682fb245f608078
後藤大輝 <daiki@D-10-19-180-71.dhcp4.washington.edu>
2018-08-20 16:42:50 -0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -30,6 +30,18 @@ io.on('connection', (socket) => {
   //   console.log('createEmail', newEmail);
   // });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -37,6 +49,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
